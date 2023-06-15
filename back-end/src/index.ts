f0e07ba5e6aca2eb48223e77f10e16369ae45dcf
Luis Henrique da Silva Carvalho <luisdev9225@gmail.com>
2023-06-15 00:31:04 -0300

    :recyle: refactor: index bug fix

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,20 +1,22 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import express from "express";
+import { createConnection } from "typeorm";
+import cors from "cors";
+import StoreRotes from "./routes/store.routes";
+import UserRoutes from "./routes/user.routes";
+const PORT = 3000;
 
-AppDataSource.initialize().then(async () => {
+async function startup() {
+  await createConnection();
+  const app = express();
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+  app.use(express.json());
+  app.use(cors());
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
+  app.use("/user", UserRoutes);
+  app.use("/store", StoreRotes);
+  app.listen(PORT, () => {
+    console.log("App running on port " + PORT);
+  });
+}
 
-    console.log("Here you can setup and run express / fastify / any other framework.")
-
-}).catch(error => console.log(error))
+startup();
